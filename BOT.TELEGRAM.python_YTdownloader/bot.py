c9e6a96b3cc4ba28ba237f5e5bd82e62bcd86ed5
i do's <136927932+id0s@users.noreply.github.com>
2026-02-23 08:50:21 +0700

    Update bot.py

--- BOT.TELEGRAM.python_YTdownloader/bot.py
+++ BOT.TELEGRAM.python_YTdownloader/bot.py
@@ -4,7 +4,7 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters
 import yt_dlp
 
-TOKEN = os.getenv("RAILWAY_TOKEN")
+TOKEN = os.getenv("BOT_TOKEN")
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
